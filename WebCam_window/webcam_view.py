b43eba6b6d6a76a0749855730d881702fa749da2
홍다경 <hdk990425@gmail.com>
2022-06-07 23:08:55 +0900

    [홍다경]video기능 구현

--- WebCam_window/webcam_view.py
+++ WebCam_window/webcam_view.py
@@ -26,18 +26,18 @@ class webCamWindow(QDialog,QWidget,form_image_window):
         self.home.clicked.connect(self.Home)
         self.connectBtn.clicked.connect(self.connectwebcam)
         self.stopBtn.clicked.connect(self.stop_webcam)
-        self.convertBtn.clicked.connect(self.convertVideo)
+        self.openVideoBtn.clicked.connect(self.openVideo)
         self.grayscaleBtn.clicked.connect(self.grayScale)
         self.flipBtn.clicked.connect(self.flipVideo)
 
-
+    # webcam 열어서 grayscale과 flip 적용하는 부분
     def connectwebcam(self):
         print('connect web cam')
         import cv2
         self.stop_webcam = False
         self.grayscale = False
-        self.convert_video = False
-        self.flip_90, self.flip_180, self.flip_270 = False, False, False
+        self.open_video = False
+        #self.flip_90, self.flip_180, self.flip_270 = False, False, False
         self.currentRotation = 0
 
         cap = cv2.VideoCapture(0)
@@ -61,11 +61,52 @@ class webCamWindow(QDialog,QWidget,form_image_window):
             cv2.waitKey(24)
             if self.stop_webcam:
                 break
-            if self.convert_video:
+            if self.open_video:
                 pass
 
+    # video 파일 열어서 grayscale과 flip 적용하는 부분
+    def openVideo(self):
+        from PyQt5 import QtWidgets, QtCore
 
+        print('connect video')
+        import cv2
+        self.stop_webcam = False
+        self.grayscale = False
+        self.open_video = False
+        # self.flip_90, self.flip_180, self.flip_270 = False, False, False
+        self.currentRotation = 0
 
+        # 동영상 파일 열기
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self,
+                                                                'Open File',
+                                                                QtCore.QDir.rootPath(),
+                                                                '*.*')
+        cap = cv2.VideoCapture(fileName)
+
+        while True:
+            ret, self.source_image = cap.read()
+            init_image = cv2.cvtColor(self.source_image, cv2.COLOR_BGR2RGB)
+            self.show_image('input', init_image)
+
+            if self.grayscale:
+                image = cv2.cvtColor(init_image, cv2.COLOR_BGR2GRAY)
+            elif self.currentRotation == 90:
+                image = cv2.rotate(init_image, cv2.ROTATE_90_CLOCKWISE)
+            elif self.currentRotation == 180:
+                image = cv2.rotate(init_image, cv2.ROTATE_180)
+            elif self.currentRotation == 270:
+                image = cv2.rotate(init_image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            else:
+                image = init_image
+            self.show_image('output', image)
+
+            cv2.waitKey(24)
+            if self.stop_webcam:
+                break
+            if self.open_video:
+                pass
+
+    # video 보여주는 부분
     def show_image(self,label,image):
         qt_image1 = QtGui.QImage(image.data,
                                  self.width,
@@ -82,16 +123,16 @@ class webCamWindow(QDialog,QWidget,form_image_window):
             pixmap = QtGui.QPixmap.fromImage(qt_image1)
             self.outputCam.setPixmap(pixmap)
 
+    # video stop 하는 부분
     def stop_webcam(self):
         self.stop_webcam = True
 
-    def convertVideo(self):
-        self.convert_video = True
-
+    # grayscale 적용하기 위한 함수
     def grayScale(self):
         self.currentRotation = 0
         self.grayscale = True
 
+    # flip 적용하기 위한 함수
     def flipVideo(self):
         self.grayscale = False
         if self.currentRotation < 360:
@@ -99,6 +140,6 @@ class webCamWindow(QDialog,QWidget,form_image_window):
             if self.currentRotation == 360:
                 self.currentRotation = 0
 
-
+    # 현재 화면 끄는 함수
     def Home(self):
         self.close()
